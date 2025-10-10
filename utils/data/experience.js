9085e490dfe6070c6eaecb5ba19609070bfb8745
Hannah Schultz <machannah@icloud.com>
2025-10-10 17:13:34 -0400

    add my basic info

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Web and Email Specialist',
+    company: "Geneva College",
+    duration: "(Aug 2024 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "IT and Database Coordinator",
+    company: "Coalition for Christian Outreach",
+    duration: "(Jan 2023 - OCt 2023)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Software Engineer I",
+    company: "Sierra Nevada Coorporation",
+    duration: "(Jul 2022 - Oct 2022)"
+  },
+  {
+    id: 4,
+    title: "Intern",
+    company: "Sierra Nevada Coorporation",
+    duration: "(Jun 2021 - Oct 2021)"
   }
 ]
