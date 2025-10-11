c3ba6d2165807ae218218732dfa05128e261ca1a
Hannah Schultz <machannah@icloud.com>
2025-10-11 15:20:19 -0400

    call education section

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ import { personalData } from "@/utils/data/personal-data";
 const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
 const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
 const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
 const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
 const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
