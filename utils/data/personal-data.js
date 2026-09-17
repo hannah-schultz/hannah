e6f7c34dd59d9480a2faafd230ede6f600d69260
hannah-schultz <machannah@icloud.com>
2026-09-16 20:05:32 -0400

    Update headshot filepath

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Hannah Schultz",
-  profile: '/Hannah Schultz-3033.jpg',
+  profile: '/hannah-headshot.jpg',
   designation: "Web Developer",
   description: "I blend code, creativity, design, and strategy to build digital experiences that are intuitive and impactful. With a background in computer science and marketing technology, I enjoy turning complex challenges into thoughtful, user-focused solutions. Whether I’m training for a race, finding a new trail, or planning a camping trip, I’m always ready to make the most of whatever adventure is ahead.",
   email: 'machannah@icloud.com',
